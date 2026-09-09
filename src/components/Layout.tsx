b74a2446d8a66ecaf4c193c808eadbb99e24e0f2
PALADUGU GANESH NAIDU <paladuguganeshnaidu@gmail.com>
2026-09-09 20:50:00 +0530

    remove unnecessary outlet remounts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
 import { Footer } from "./Footer"
 import { ContactDock } from "./ContactDock"
@@ -6,15 +6,13 @@ import { Seo } from "./Seo"
 import { ScrollToTop } from "./ScrollToTop"
 
 export function Layout() {
-  const { pathname } = useLocation()
-
   return (
     <>
       <ScrollToTop />
       <Seo />
       <Header />
       <main>
-        <Outlet key={pathname} />
+        <Outlet />
       </main>
       <Footer />
       <ContactDock />
